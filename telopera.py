3bbbf8a97a329ad0465b217134c110dd9be26f66
unknown <hanmg@buaa.edu.cn>
2024-12-04 11:14:36 +0800

    clearly

--- telopera.py
+++ telopera.py
@@ -51,71 +51,85 @@ angle_range = (c_float*18)()
 data_disp = 1
 timestamp = ctypes.c_uint
 
-lib.wgInit.restype = ctypes.c_bool
+
 #g_pGlove=lib.wgInitManu(3,115200)
 
 ################################################ data glove init ################################################
-g_pGlove=lib.wgInit(1)  # 0=左手,1=右手,-1=不分左右 
 
-if g_pGlove == False:
-    print("No Glove")
-    lib.wgClose()
+def init_glove():
+    lib.wgInit.restype = ctypes.c_bool
+    g_pGlove=lib.wgInit(1)  # 0=左手,1=右手,-1=不分左右 
+    if g_pGlove == False:
+        print("No Glove")
+        lib.wgClose()
 
-else:
-    lib.wgGetNumOfSensor.restype = ctypes.c_int
-    num_sensor = lib.wgGetNumOfSensor()
-    print("glove connected")
-    print(f'num_sensor: {num_sensor}')
-    sn_str = b'aaaaaaaaa'
-    snfunc(sn_str)
-    print(f"该手套序列号为：{sn_str.decode('utf-8')}")
-# print(regdict)
+    else:
+        lib.wgGetNumOfSensor.restype = ctypes.c_int
+        num_sensor = lib.wgGetNumOfSensor()
+        print("glove connected")
+        print(f'num_sensor: {num_sensor}')
+        sn_str = b'aaaaaaaaa'
+        snfunc(sn_str)
+        print(f"该手套序列号为：{sn_str.decode('utf-8')}")
+    # print(regdict)
 
-#lib.wgSetCalibMode(ctypes.c_int(0))#自动标定
+    #lib.wgSetCalibMode(ctypes.c_int(0))#自动标定
 
-lib.wgResetCalib()
-lib.wgGetCalibRange.restype = ctypes.c_float
-for i in range(18):
-    angle_range[i] = lib.wgGetCalibRange(ctypes.c_int(i))
-    # print(f"angle_range[{i+1}]: {angle_range[i]}")
+    lib.wgResetCalib()
+    lib.wgGetCalibRange.restype = ctypes.c_float
+    for i in range(18):
+        angle_range[i] = lib.wgGetCalibRange(ctypes.c_int(i))
+        # print(f"angle_range[{i+1}]: {angle_range[i]}")
 
-# for key,value in joints.items():
-#         print (f"{key}  ",end="")
-# print('\n')
+    # for key,value in joints.items():
+    #         print (f"{key}  ",end="")
+    # print('\n')
 
 ################################################ inspire hand init ################################################
-print("打开灵巧手串口")
-ser = openSerial('COM6', 115200)
-forceClb(ser,1)
-inspire_ready = False
+def init_inspire():
+    print("打开灵巧手串口")
+    ser = openSerial('COM6', 115200)
+    forceClb(ser,1)
+    inspire_ready = False
+    return ser, inspire_ready
 
 ################################################# main loop #################################################
-while True:
-    if msvcrt.kbhit():
-        key=msvcrt.getch()
-        #print(type(key))
-        keystr=str(key, encoding="utf-8")
-        if (keystr=='q'):
-                break
-        if (keystr=='r'):
-            lib.wgResetCalib()
-            for i in range(18):
-                angle_range[i] = lib.wgGetCalibRange(ctypes.c_int(i))
-        if (keystr=='c'):
-            inspire_ready = True
+def teleop(ser, inspire_ready):
+    while True:
+        if msvcrt.kbhit():
+            key=msvcrt.getch()
+            #print(type(key))
+            keystr=str(key, encoding="utf-8")
+            if (keystr=='q'):
+                    break
+            if (keystr=='r'):
+                lib.wgResetCalib()
+                for i in range(18):
+                    angle_range[i] = lib.wgGetCalibRange(ctypes.c_int(i))
+            if (keystr=='c'):
+                inspire_ready = True
 
-    lib.wgGetAngle.restype = ctypes.c_uint
-    timestamp = lib.wgGetAngle(angle)
-    write_angle = init_pos
+        lib.wgGetAngle.restype = ctypes.c_uint
+        timestamp = lib.wgGetAngle(angle)
+        write_angle = init_pos
 
-    write_angle[0] = int(1000-(angle[joints["Pin_MCP"] - 1]/angle_range[joints["Pin_MCP"] - 1] * 1000))
-    write_angle[1] = int(1000-(angle[joints["Rin_MCP"] - 1]/angle_range[joints["Rin_MCP"] - 1] * 1000))
-    write_angle[2] = int(1000-(angle[joints["Mid_MCP"] - 1]/angle_range[joints["Mid_MCP"] - 1] * 1000))
-    write_angle[3] = int(1000-(angle[joints["Ind_MCP"] - 1]/angle_range[joints["Ind_MCP"] - 1] * 1000))
-    write_angle[4] = int(1000-(angle[joints["Thu_MCP"] - 1]/angle_range[joints["Thu_MCP"] - 1] * 1000))
-    write_angle[5] = int(1000-(angle[joints["Thu_CMC"] - 1]/angle_range[joints["Thu_CMC"] - 1] * 1000))
-    print(f"Pin: {write_angle[0]}   Rin: {write_angle[1]}   Mid: {write_angle[2]}   Ind: {write_angle[3]}  Thu: {write_angle[4]}  Thu_CMC: {write_angle[5]}")
+        write_angle[0] = int(1000-(angle[joints["Pin_MCP"] - 1]/angle_range[joints["Pin_MCP"] - 1] * 1000))
+        write_angle[1] = int(1000-(angle[joints["Rin_MCP"] - 1]/angle_range[joints["Rin_MCP"] - 1] * 1000))
+        write_angle[2] = int(1000-(angle[joints["Mid_MCP"] - 1]/angle_range[joints["Mid_MCP"] - 1] * 1000))
+        write_angle[3] = int(1000-(angle[joints["Ind_MCP"] - 1]/angle_range[joints["Ind_MCP"] - 1] * 1000))
+        write_angle[4] = int(1000-(angle[joints["Thu_MCP"] - 1]/angle_range[joints["Thu_MCP"] - 1] * 1000))
+        write_angle[5] = int(1000-(angle[joints["Thu_CMC"] - 1]/angle_range[joints["Thu_CMC"] - 1] * 1000))
+        print(f"Pin: {write_angle[0]}   Rin: {write_angle[1]}   Mid: {write_angle[2]}   Ind: {write_angle[3]}  Thu: {write_angle[4]}  Thu_CMC: {write_angle[5]}")
 
-    if timestamp > 0 and inspire_ready:
-        write_data_6(ser,1,'angleSet',write_angle)
+        if timestamp > 0 and inspire_ready:
+            write_data_6(ser,1,'angleSet',write_angle)
 
+
+if __name__ == '__main__':
+    init_glove()
+    ser, inspire_ready = init_inspire()
+    teleop(ser, inspire_ready)
+    lib.wgClose()
+    ser.close()
+    print("程序结束")
+    sys.exit(0)
